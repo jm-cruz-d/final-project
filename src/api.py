93ec5b6a60105f2577a2ab9d67dbe36a0d0da7ca
jm <jm.cruz.don@gmail.com>
2019-12-18 14:07:41 +0100

    pretty api

--- src/api.py
+++ src/api.py
@@ -9,23 +9,56 @@ app = Flask(__name__)
 # Carpeta de subida
 app.config['UPLOAD_FOLDER'] = './fotos_test'
 
+
 @app.route("/")
 def upload_file():
- # renderiamos la plantilla "formulario.html"
- return render_template('formulario.html')
+    # renderiamos la plantilla "formulario.html"
+    return render_template('formulario.html')
+
 
 @app.route("/upload", methods=['POST'])
 def uploader():
- if request.method == 'POST':
-  # get image from input
-  f = request.files['archivo']
-  filename = secure_filename(f.filename)
-  # save image
-  f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-  # Predicting image saved
-  pred = pr.pred(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-  return jsonify(pred)
+    if request.method == 'POST':
+        # get image from input
+        f = request.files['archivo']
+        filename = secure_filename(f.filename)
+        # save image
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+    pics = {
+        'Atocha': {'photo': "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Estaci%C3%B3n_de_Atocha_-_felipe_gabaldon.jpg/1200px-Estaci%C3%B3n_de_Atocha_-_felipe_gabaldon.jpg",
+                   'url': "https://es.wikipedia.org/wiki/Estaci%C3%B3n_de_Atocha"},
+        'Monumento Alfonso XII': {'photo': "https://www.esmadrid.com/sites/default/files/styles/content_type_full/public/recursosturisticos/infoturistica/monumento_alfonso_xii_carmen.jpg?itok=CAcpXoA3",
+                                  'url': "https://es.wikipedia.org/wiki/Monumento_a_Alfonso_XII_de_Espa%C3%B1a"},
+        'Museo Prado': {'photo': "https://aws-tiqets-cdn.imgix.net/images/content/91e400c23f0140abb059eb1958530e84.jpg?auto=format&fit=crop&ixlib=python-1.1.2&q=25&s=5cab2e6922dce4aef05898c67c6b3648&w=400&h=320&dpr=2.625",
+                        'url': "https://es.wikipedia.org/wiki/Museo_del_Prado"},
+        'Edificio Schweppes': {'photo': "https://madridsecreto.co/wp-content/uploads/2018/11/portada-capitol.jpg",
+                               'url': "https://es.wikipedia.org/wiki/Edificio_Carri%C3%B3n"},
+        'Plaza Mayor': {'photo': "https://www.esmadrid.com/sites/default/files/styles/content_type_full/public/recursosturisticos/infoturistica/panaderia_1399451332.767.jpg?itok=a_DAF8_a",
+                        'url': "https://es.wikipedia.org/wiki/Casa_de_la_Panader%C3%ADa"},
+        'Catedral de La Almudena': {'photo': "https://www.esmadrid.com/sites/default/files/styles/content_type_full/public/recursosturisticos/infoturistica/catedralalmudena2ok.jpg?itok=0hv37a5F",
+                                    'url': "https://es.wikipedia.org/wiki/Catedral_de_la_Almudena"},
+        'Ayuntamiento de Madrid': {'photo': "http://images.telemadrid.es/2019/04/13/noticias/madrid/Ayuntamiento-Madrid-compra-palacete-Carabanchel_2112398742_6930059_1300x731.jpg",
+                                   'url': "https://es.wikipedia.org/wiki/Palacio_de_Cibeles"},
+        'Templo de Debod': {'photo': "https://live.staticflickr.com/881/40547456784_fcba0cd06d_b.jpg",
+                            'url': "https://es.wikipedia.org/wiki/Templo_de_Debod"},
+        'Puerta Alcala': {'photo': "https://cheapinmadrid.es/wp-content/uploads/2018/02/puerta-alcala.jpg",
+                          'url': "https://es.wikipedia.org/wiki/Puerta_de_Alcal%C3%A1"},
+        'Plaza de toros Las Ventas': {'photo': "https://upload.wikimedia.org/wikipedia/commons/1/16/Plaza_de_Toros_de_Las_Ventas_%28Madrid%2C_Espa%C3%B1a%29.jpg",
+                                      'url': "https://es.wikipedia.org/wiki/Las_Ventas"},
+        'Banco España': {'photo': "https://e00-expansion.uecdn.es/assets/multimedia/imagenes/2017/06/16/14976383172511.jpg",
+                         'url': "https://es.wikipedia.org/wiki/Banco_de_Espa%C3%B1a"},
+        'Jardin Botanico': {'photo': "https://www.esmadrid.com/sites/default/files/styles/content_type_full/public/recursosturisticos/infoturistica/JARDINBOTANICO_006_alta.jpg?itok=XSqurbMY",
+                            'url': "https://es.wikipedia.org/wiki/Real_Jard%C3%ADn_Bot%C3%A1nico_de_Madrid"},
+        'Palacio Cristal': {'photo': "https://madridsecreto.co/wp-content/uploads/2018/04/palacio-cristal-retiro-madrid.jpg",
+                            'url': "https://es.wikipedia.org/wiki/Palacio_de_Cristal_del_Retiro"}
+    }
+
+    # Predicting image saved
+    pred = pr.pred(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return render_template('form_pred.html', pred=pred, pics=pics)
+
 
 if __name__ == '__main__':
- # Iniciamos la aplicación
- app.run(debug=True, threaded=False)
+    # Iniciamos la aplicación
+    app.run(debug=True, threaded=False)
--- src/predict.py
+++ src/predict.py
@@ -5,9 +5,9 @@ import tensorflow as tf
 from keras import backend as K
 
 # Classes
-dirs = ['atocha', 'alfonso_xii', 'museo_prado', 'schweppes', 
-        'plaza_mayor', 'la_almudena', 'ayuntamiento', 'debod', 
-        'puerta_alcala', 'ventas', 'banco_esp', 'jardin_botanico', 'palacio_cristal']
+dirs = ['Atocha', 'Monumento Alfonso XII', 'Museo Prado', 'Edificio Schweppes', 
+        'Plaza Mayor', 'Catedral de La Almudena', 'Ayuntamiento de Madrid', 'Templo de Debod', 
+        'Puerta Alcala', 'Plaza de toros Las Ventas', 'Banco España', 'Jardin Botanico', 'Palacio Cristal']
 
 # Loading model
 model = load_model('../models/model(0.96).hdf5')
@@ -22,13 +22,19 @@ def pred(path):
     imres = cv2.resize(im,(256,256))
     
     global tf_graph,tf_session
-
+    
     with tf_session.as_default():
             with tf_graph.as_default():
 
                 pred = model.predict(np.expand_dims(imres,axis=0))[0]
                 for i, p in enumerate(pred):
                     if p > 0.5:
-                        return {dirs[i]: str(p)}
+                        return {
+                            "classes":dirs,
+                            "prediction":{
+                                "label":dirs[i],
+                                "prob": str(p)
+                            }
+                        }
 
 #print(pred(path))
